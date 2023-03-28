a113bfec8890730027aba57a6e4422bafc8e5d5a
hezroneokoth <hezrone.okoth@icloud.com>
2023-03-28 01:25:43 -0700

    Updated Task10

--- get_precision.c
+++ get_precision.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdarg.h>
 
 /**
  * get_precision - Calculates the precision for printing
@@ -21,7 +20,7 @@ int get_precision(const char *format, int *i, va_list list)
 
 	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
 	{
-		if (isdigit(format[curr_i]))
+		if (is_digit(format[curr_i]))
 		{
 			precision *= 10;
 			precision += format[curr_i] - '0';
